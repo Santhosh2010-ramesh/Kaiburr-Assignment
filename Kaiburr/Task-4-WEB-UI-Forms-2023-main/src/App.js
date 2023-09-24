1ff2ac2ca0b28fd91c3645ef38fc799b054b4fd2
Santhosh2010-ramesh <68174618+Santhosh2010-ramesh@users.noreply.github.com>
2023-09-24 22:01:35 +0530

    Add files via upload

---
+++ Kaiburr/Task-4-WEB-UI-Forms-2023-main/src/App.js
@@ -0,0 +1,13 @@
+import Appbar from "./Compo/Appbar.js";
+import MainBody from './Compo/MainBody.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <MainBody/>
+    </div>
+  );
+}
+
+export default App;
